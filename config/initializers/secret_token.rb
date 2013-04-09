ed9eb1ee261a70c29b5bb1b17f311b5d0c4e3200
John Carlo D. Perez <johncarloperez002@yahoo.com>
2013-04-09 22:27:14 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AptTeam::Application.config.secret_token = 'edea6bd73ed699335a0acb6316d7ef7fb319447dceaaa0a04194f8ca8a2a3905b5103ad125c57800979c40ef3d6837d3d5950405cb74f4da78dcba4b2762ae5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AptTeam::Application.config.session_store :cookie_store, key: '_AptTeam_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AptTeam::Application.config.session_store :active_record_store
